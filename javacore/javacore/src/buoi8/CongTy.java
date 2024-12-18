39e75bd7708707bc5d35d001987cd9f7ddc6cdc1
NguyenKien-2003 <nguyenngockien30082003@gmail.com>
2024-12-18 13:27:28 +0700

    bai test thuc hanh

---
+++ javacore/javacore/src/buoi8/CongTy.java
@@ -0,0 +1,31 @@
+package buoi8;
+
+public class CongTy {
+    private String tenCongTy;
+    private String maSoThue;
+    private long doanhThuThang;
+
+    public String getTenCongTy() {
+        return tenCongTy;
+    }
+
+    public void setTenCongTy(String tenCongTy) {
+        this.tenCongTy = tenCongTy;
+    }
+
+    public String getMaSoThue() {
+        return maSoThue;
+    }
+
+    public void setMaSoThue(String maSoThue) {
+        this.maSoThue = maSoThue;
+    }
+
+    public long getDoanhThuThang() {
+        return doanhThuThang;
+    }
+
+    public void setDoanhThuThang(long doanhThuThang) {
+        this.doanhThuThang = doanhThuThang;
+    }
+}
